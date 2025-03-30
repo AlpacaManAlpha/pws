32f35031bde7850e7f7e84be19e7640b4b4aeafa
alpaca <153322594+AlpacaManAlpha@users.noreply.github.com>
2025-03-30 16:48:45 +0200

    Update Restore_RightClick_W11.ps1

--- ClientManagement/Restore_RightClick_W11.ps1
+++ ClientManagement/Restore_RightClick_W11.ps1
@@ -1,7 +1,8 @@
+# Restore traditional right-click menu for Windows 11
+
 # Add a registry key in the specified location
+reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
 # Restart the explorer process
-
-reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 Stop-Process -Name explorer -Force
 Start-Process explorer
